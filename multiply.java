75a92bbfcf39ed9ffef0b9f5373c1d82b4336652
Shivam Ahuja <ahujashiva007@gmail.com>
2023-05-03 23:35:09 +0530

    Third Commit

---
+++ multiply.java
@@ -0,0 +1,9 @@
+public class multiply {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 10;
+        int product = num1 * num2;
+
+        System.out.println("The product of " + num1 + " and " + num2 + " is " + product);
+    }
+}
